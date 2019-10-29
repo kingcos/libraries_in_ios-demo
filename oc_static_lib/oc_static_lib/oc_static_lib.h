1a4086d5444850f535b106c9db19d744302f1425
kingcos <2821836721v@gmail.com>
2019-10-29 19:29:05 +0800

    Add oc_static_lib

---
+++ oc_static_lib/oc_static_lib/oc_static_lib.h
@@ -0,0 +1,13 @@
+//
+//  oc_static_lib.h
+//  oc_static_lib
+//
+//  Created by kingcos on 2019/10/29.
+//  Copyright © 2019 kingcos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface oc_static_lib : NSObject
+
+@end
---
+++ oc_static_lib/oc_static_lib/oc_static_lib.m
@@ -0,0 +1,13 @@
+//
+//  oc_static_lib.m
+//  oc_static_lib
+//
+//  Created by kingcos on 2019/10/29.
+//  Copyright © 2019 kingcos. All rights reserved.
+//
+
+#import "oc_static_lib.h"
+
+@implementation oc_static_lib
+
+@end
