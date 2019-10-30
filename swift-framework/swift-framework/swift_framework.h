37b8855b35e47a694228c8ccfd3f745e634b7b5a
kingcos <2821836721v@gmail.com>
2019-10-30 19:51:34 +0800

    Add swift-framework

---
+++ swift-framework/swift-framework/swift_framework.h
@@ -0,0 +1,18 @@
+//
+//  swift_framework.h
+//  swift-framework
+//
+//  Created by kingcos on 2019/10/30.
+//  Copyright © 2019 kingcos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for swift_framework.
+FOUNDATION_EXPORT double swift_frameworkVersionNumber;
+
+//! Project version string for swift_framework.
+FOUNDATION_EXPORT const unsigned char swift_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <swift_framework/PublicHeader.h>
+
