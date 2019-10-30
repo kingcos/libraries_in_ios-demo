bcc766522c4eff995c02bc80a1c72c1f36072a04
kingcos <2821836721v@gmail.com>
2019-10-30 19:43:29 +0800

    Add oc-framework products

---
+++ oc-framework/Products/Debug-iphonesimulator/oc_framework.framework/Headers/oc_framework.h
@@ -0,0 +1,18 @@
+//
+//  oc_framework.h
+//  oc-framework
+//
+//  Created by kingcos on 2019/10/30.
+//  Copyright © 2019 kingcos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for oc_framework.
+FOUNDATION_EXPORT double oc_frameworkVersionNumber;
+
+//! Project version string for oc_framework.
+FOUNDATION_EXPORT const unsigned char oc_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <oc_framework/PublicHeader.h>
+#import <oc_framework/PublicHeader.h>
