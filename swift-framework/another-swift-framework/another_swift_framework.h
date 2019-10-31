5d72fff032f9033404eba09c1a5a49f24f9b42da
kingcos <2821836721v@gmail.com>
2019-10-31 18:02:56 +0800

    Add Aggregate Target

---
+++ swift-framework/another-swift-framework/another_swift_framework.h
@@ -0,0 +1,18 @@
+//
+//  another_swift_framework.h
+//  another-swift-framework
+//
+//  Created by kingcos on 2019/10/31.
+//  Copyright © 2019 kingcos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for another_swift_framework.
+FOUNDATION_EXPORT double another_swift_frameworkVersionNumber;
+
+//! Project version string for another_swift_framework.
+FOUNDATION_EXPORT const unsigned char another_swift_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <another_swift_framework/PublicHeader.h>
+
---
+++ swift-framework/swift-framework/AnotherFramework/Swift_Bar.swift
@@ -0,0 +1,15 @@
+//
+//  Swift_Bar.swift
+//  another-swift-framework
+//
+//  Created by kingcos on 2019/10/31.
+//  Copyright © 2019 kingcos. All rights reserved.
+//
+
+import Foundation
+
+@objc public class Swift_Bar : NSObject {
+    @objc public func bar() {
+        print("Swift_Bar - \(#function)")
+    }
+}
