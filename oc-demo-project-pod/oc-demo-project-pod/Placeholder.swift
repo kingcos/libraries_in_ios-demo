ef59dff5025fc414591e9b80f3127a45d2b34af7
kingcos <2821836721v@gmail.com>
2019-11-04 16:54:17 +0800

    Add swift-pod

---
+++ oc-demo-project-pod/oc-demo-project-pod/Placeholder.swift
@@ -0,0 +1,9 @@
+//
+//  Placeholder.swift
+//  oc-demo-project-pod
+//
+//  Created by 买明 on 2019/11/4.
+//  Copyright © 2019 kingcos. All rights reserved.
+//
+
+import Foundation
---
+++ swift-pod/swift-pod/Classes/Swift_Foo.swift
@@ -0,0 +1,20 @@
+//
+//  Swift_Foo.swift
+//  swift-framework
+//
+//  Created by kingcos on 2019/10/30.
+//  Copyright © 2019 kingcos. All rights reserved.
+//
+
+import Foundation
+
+@objc public class Swift_Foo : NSObject {
+    @objc public func foo() {
+        print("Swift_Foo - \(#function)")
+    }
+
+    @objc public func strings() -> String {
+        let bundle = Bundle(for: Swift_Foo.self)
+        return NSLocalizedString("my_blog", bundle: bundle, comment: "My blog link.")
+    }
+}
